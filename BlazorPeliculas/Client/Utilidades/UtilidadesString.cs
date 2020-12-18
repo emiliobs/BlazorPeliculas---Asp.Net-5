b4b21e6e7f8891b78f81c0648400b940730ceaa9
Emilio Barrea <barrera_emilio@yahoo.es>
2020-12-18 13:17:40 +0000

    Compoenets with Parameters

--- BlazorPeliculas/Client/Utilidades/UtilidadesString.cs
+++ BlazorPeliculas/Client/Utilidades/UtilidadesString.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BlazorPeliculas.Client.Utilidades
+﻿namespace BlazorPeliculas.Client.Utilidades
 {
     public class UtilidadesString
     {
-        public static string EnMayuscular(string valor) => valor.ToUpper();
+        public static string EnMayuscular(string valor)
+        {
+            return valor.ToUpper();
+        }
     }
 }
