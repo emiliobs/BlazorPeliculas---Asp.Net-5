408cf97ada1bd72030e1fb46d79d7a45a59eee5d
Emilio Barrea <barrera_emilio@yahoo.es>
2020-12-19 19:46:59 +0000

    Clases PArciales - Separando Códogo c# y UI

---
+++ BlazorPeliculas/Client/Pages/Counter.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPeliculas.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject]
+        private ServiciosSingleton ServiciosSingleton { get; set; }
+
+        [Inject]
+        private ServiciosTransient ServiciosTransient { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            ServiciosSingleton.Valor = currentCount;
+            ServiciosTransient.Valor = currentCount;
+        }
+    }
+}
