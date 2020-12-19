d904b4ab981ba661644ae0ff7d368524c21d4af3
Emilio Barrea <barrera_emilio@yahoo.es>
2020-12-19 20:11:44 +0000

    INvocar métodos desde C#

---
+++ BlazorPeliculas/Client/Helper/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Helper
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime jS, string mensaje)
+        {
+            await jS.InvokeVoidAsync("console.log","Pruena de console log");
+
+            return await jS.InvokeAsync<bool>("confirm", mensaje);
+        }
+    }
+}
